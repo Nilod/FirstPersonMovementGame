6609961e79a5c542e989a8ca7d26eedfafdf5f29
Nilo <nilo.duvignau@gmail.com>
2025-12-19 20:21:12 +0100

    Ground chek workin

--- Assets/PlayerMovement.cs
+++ Assets/PlayerMovement.cs
@@ -1,3 +1,4 @@
+using NUnit.Framework;
 using UnityEngine;
 using UnityEngine.InputSystem;
 
@@ -12,32 +13,31 @@ public class PlayerMovement : MonoBehaviour
     public float jumpForce;
     public int maxJumps = 1;
 
-    [Header("Surface Checking")]
-    public float maxSlopeAngle = 30f;
+    [Header("Ground Check")]
+    //public float maxSlopeAngle = 30f;
+    public float sphereRadius = 0.5f;
+    public float shpereRelativeHeight = 0f;
+    public float groundCheckDistance = 0.01f;
+    private Vector3 groundDetectionHeight;
 
     private Transform playerTransform;
     private Rigidbody playerRigidbody;
-
-    /*
-    private PlayerInput playerInput;
-    private InputAction moveAction;
-    private InputAction jumpAction;
-    */
     private Vector2 moveInput;
-    private bool requestJump;
+    private bool jumpRequested;
 
     private bool isGrounded;
+    private int jumpsRemaining;
+    private LayerMask notPlayerLayer;
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
         playerRigidbody = GetComponent<Rigidbody>();
         playerTransform = GetComponent<Transform>();
-        /*
-        playerInput = GetComponent<PlayerInput>();
-        moveAction = playerInput.actions["Move"];
-        jumpAction = playerInput.actions["Jump"];
-        */
+        jumpsRemaining = maxJumps;
+        groundDetectionHeight = new Vector3(0f, shpereRelativeHeight, 0f);
+
+        notPlayerLayer = ~LayerMask.GetMask("Player");
     }
 
     // Update is called once per frame
@@ -48,12 +48,20 @@ public class PlayerMovement : MonoBehaviour
 
     void FixedUpdate()
     {
-        Move();
-        if (requestJump && isGrounded)
+        Debug.Log("Is Grounded: " + isGrounded);
+        GroundCheck();
+        if (isGrounded)
         {
-            Jump();
-            requestJump = false;
+            Move();
+            if (jumpRequested)
+            {
+                Jump();
+                jumpRequested = false;
+                jumpsRemaining--;
+            }
         }
+        
+        
     }
 
     public void OnMove(InputAction.CallbackContext context)
@@ -65,11 +73,11 @@ public class PlayerMovement : MonoBehaviour
     {
         if (context.performed)
         {
-            requestJump = true;
+            jumpRequested = true;
         }
         else if (context.canceled)
         {
-            requestJump = false;
+            jumpRequested = false;
         }
         //isJumping = context.ReadValue<float>() > 0;
     }
@@ -86,51 +94,59 @@ public class PlayerMovement : MonoBehaviour
         //force.x = Mathf.Sqrt(Mathf.Log(Mathf.Abs(velocityDif.x) + 1) * 0.5f*Mathf.Abs(velocityDif.x)) * Mathf.Sign(velocityDif.x);
         //force.z = Mathf.Sqrt(Mathf.Log(Mathf.Abs(velocityDif.z) + 1) * 0.5f*Mathf.Abs(velocityDif.z)) * Mathf.Sign(velocityDif.z);
 
-        playerRigidbody.AddForce(force, ForceMode.Acceleration); //ForceMode.Force, ForceMode.Acceleration, ForceMode.VelocityChange
+        playerRigidbody.AddForce(force, ForceMode.Force); //ForceMode.Force, ForceMode.Acceleration, ForceMode.VelocityChange
     }
 
     public void Jump()
     {
         if (playerRigidbody.linearVelocity.y < -1f)
-            {
-                playerRigidbody.linearVelocity = new Vector3(playerRigidbody.linearVelocity.x, -1*Mathf.Log(Mathf.Abs(playerRigidbody.linearVelocity.y)), playerRigidbody.linearVelocity.z);
-            }
-            //playerRigidbody.linearVelocity = new Vector3(playerRigidbody.linearVelocity.x, 0, playerRigidbody.linearVelocity.z);
-            playerRigidbody.AddForce(Vector3.up * jumpForce, ForceMode.Impulse);
+        {
+            playerRigidbody.linearVelocity = new Vector3(playerRigidbody.linearVelocity.x, -1*Mathf.Log(Mathf.Abs(playerRigidbody.linearVelocity.y)), playerRigidbody.linearVelocity.z);
+        }
+        //playerRigidbody.linearVelocity = new Vector3(playerRigidbody.linearVelocity.x, 0, playerRigidbody.linearVelocity.z);
+        playerRigidbody.AddForce(Vector3.up * jumpForce, ForceMode.Impulse);
     }
 
-/*
-    public bool IsGrounded()
+    public void GroundCheck()
     {
-        //return Physics.SphereCast(playerTransform.position, groundDetectionSphereRadius, Vector3.down, out RaycastHit hitInfo, 1.1f, QueryTriggerInteraction.Ignore);
+        isGrounded = Physics.SphereCast(
+            transform.position + groundDetectionHeight,
+            sphereRadius, 
+            Vector3.down, 
+            out RaycastHit hitInfo, 
+            sphereRadius + groundCheckDistance, 
+            notPlayerLayer, 
+            QueryTriggerInteraction.Ignore);
+    }
+
+/* Ground check from the tutorial
+    public void GroundCheck()
+    {
+        Collider[] colliders;
+
+        Vector3 spherePosition = transform.position + sphereOffset;
+        colliders = Physics.OverlapSphere(spherePosition, sphereRadius, notPlayerLayer);
+
+        if (colliders.Length == 0)
+        {
+            isGrounded = false;
+            Debug.Log("No colliders detected.");
+        }
+
+        if (colliders.Length > 0)
+        {
+            foreach (Collider col in colliders)
+            {
+                Vector3 colliderPosition = col.gameObject.transform.position;
+
+                float groundAngle = Vector3.Angle(spherePosition, colliderPosition);
+
+                if (groundAngle > 180f + maxSlopeAngle || groundAngle < 180f - maxSlopeAngle)
+                {
+                    isGrounded = true;
+                }
+            }
+        }
     }
 */
-
-    void OnCollisionEnter(Collision collision)
-    {
-        foreach (ContactPoint contact in collision.contacts)
-        {
-            Vector3 normal = contact.normal;
-            float angle = Vector3.Angle(contact.normal, Vector3.up);
-
-            if (angle <= maxSlopeAngle)
-            {
-                isGrounded = true;
-            }
-        }
-    }
-
-    void OnCollisionExit(Collision collision)
-    {
-        foreach (ContactPoint contact in collision.contacts)
-        {
-            Vector3 normal = contact.normal;
-            float angle = Vector3.Angle(contact.normal, Vector3.up);
-
-            if (angle <= maxSlopeAngle)
-            {
-                isGrounded = false;
-            }
-        }
-    }
 }
