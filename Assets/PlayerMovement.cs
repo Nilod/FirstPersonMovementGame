0ea91be87ceaaaa4fd423c8a166c005a93185acd
Nilo <nilo.duvignau@gmail.com>
2025-12-20 20:28:05 +0100

    V0.2 improve grounded movement

--- Assets/PlayerMovement.cs
+++ Assets/PlayerMovement.cs
@@ -1,4 +1,5 @@
 using NUnit.Framework;
+using Unity.VisualScripting;
 using UnityEngine;
 using UnityEngine.InputSystem;
 
@@ -6,12 +7,13 @@ public class PlayerMovement : MonoBehaviour
 {
     [Header("Movement")]
     public float moveSpeed;
+    public float maxSpeedChange;
     public float acceleration;
     public float deceleration;
 
     [Header("Jump")]
     public float jumpForce;
-    public int maxJumps = 1;
+    public int maxAirJumps = 1;
 
     [Header("Ground Check")]
     //public float maxSlopeAngle = 30f;
@@ -26,7 +28,7 @@ public class PlayerMovement : MonoBehaviour
     private bool jumpRequested;
 
     private bool isGrounded;
-    private int jumpsRemaining;
+    private int airJumpsRemaining;
     private LayerMask notPlayerLayer;
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
@@ -34,7 +36,7 @@ public class PlayerMovement : MonoBehaviour
     {
         playerRigidbody = GetComponent<Rigidbody>();
         playerTransform = GetComponent<Transform>();
-        jumpsRemaining = maxJumps;
+        airJumpsRemaining = maxAirJumps;
         groundDetectionHeight = new Vector3(0f, shpereRelativeHeight, 0f);
 
         notPlayerLayer = ~LayerMask.GetMask("Player");
@@ -49,19 +51,28 @@ public class PlayerMovement : MonoBehaviour
     void FixedUpdate()
     {
         Debug.Log("Is Grounded: " + isGrounded);
+        Debug.Log("Velocity: " + Vector3.Magnitude(playerRigidbody.linearVelocity));
         GroundCheck();
         if (isGrounded)
         {
-            Move();
+            airJumpsRemaining = maxAirJumps;
+            GroundMove();
             if (jumpRequested)
             {
-                Jump();
+                GroundJump();
                 jumpRequested = false;
-                jumpsRemaining--;
             }
         }
-        
-        
+        else
+        {
+            // AirMove();
+            if (jumpRequested && airJumpsRemaining > 0)
+            {
+                AirJump();
+                jumpRequested = false;
+                airJumpsRemaining--;
+            }
+        }   
     }
 
     public void OnMove(InputAction.CallbackContext context)
@@ -82,28 +93,43 @@ public class PlayerMovement : MonoBehaviour
         //isJumping = context.ReadValue<float>() > 0;
     }
 
-    public void Move()
+    public void GroundMove()
     {
-        Vector3 targetVelocity = (moveInput.x * playerTransform.right + moveInput.y * playerTransform.forward) * moveSpeed;
+        Vector3 targetVelocity = new Vector3(moveInput.x, 0f,  moveInput.y) * moveSpeed;
+        targetVelocity = playerTransform.TransformDirection(targetVelocity);
         Vector3 curentVelocity = playerRigidbody.linearVelocity;
         curentVelocity.y = 0f; // Ignore y velocity
         Vector3 velocityDif = targetVelocity - curentVelocity;
-        float accelRate = (targetVelocity.magnitude > 0f) ? acceleration : deceleration;
 
-        Vector3 force = velocityDif * accelRate;
-        //force.x = Mathf.Sqrt(Mathf.Log(Mathf.Abs(velocityDif.x) + 1) * 0.5f*Mathf.Abs(velocityDif.x)) * Mathf.Sign(velocityDif.x);
-        //force.z = Mathf.Sqrt(Mathf.Log(Mathf.Abs(velocityDif.z) + 1) * 0.5f*Mathf.Abs(velocityDif.z)) * Mathf.Sign(velocityDif.z);
+        float accelRate = (Vector3.Dot(curentVelocity, targetVelocity) > 0f) ? acceleration : deceleration; // 90° 
 
-        playerRigidbody.AddForce(force, ForceMode.Force); //ForceMode.Force, ForceMode.Acceleration, ForceMode.VelocityChange
+        Vector3 force = new Vector3();
+        force.x = Mathf.Log(Mathf.Abs(velocityDif.x) + 1) * accelRate * Mathf.Sign(velocityDif.x);
+        force.z = Mathf.Log(Mathf.Abs(velocityDif.z) + 1) * accelRate * Mathf.Sign(velocityDif.z);
+        // sqrt(log(x+1)*a*x)
+
+        Vector3.ClampMagnitude(force, maxSpeedChange);
+
+        playerRigidbody.AddForce(force, ForceMode.VelocityChange); //ForceMode.Force, ForceMode.Acceleration, ForceMode.VelocityChange
     }
 
-    public void Jump()
+    public void GroundJump()
+    {
+        playerRigidbody.linearVelocity = new Vector3(playerRigidbody.linearVelocity.x, 0f, playerRigidbody.linearVelocity.z); 
+        playerRigidbody.AddForce(Vector3.up * jumpForce, ForceMode.Impulse);
+    }
+
+    public void AirJump()
     {
         if (playerRigidbody.linearVelocity.y < -1f)
         {
             playerRigidbody.linearVelocity = new Vector3(playerRigidbody.linearVelocity.x, -1*Mathf.Log(Mathf.Abs(playerRigidbody.linearVelocity.y)), playerRigidbody.linearVelocity.z);
         }
-        //playerRigidbody.linearVelocity = new Vector3(playerRigidbody.linearVelocity.x, 0, playerRigidbody.linearVelocity.z);
+        else
+        {
+            playerRigidbody.linearVelocity = new Vector3(playerRigidbody.linearVelocity.x, 0f, playerRigidbody.linearVelocity.z);
+        }
+
         playerRigidbody.AddForce(Vector3.up * jumpForce, ForceMode.Impulse);
     }
 
@@ -118,35 +144,4 @@ public class PlayerMovement : MonoBehaviour
             notPlayerLayer, 
             QueryTriggerInteraction.Ignore);
     }
-
-/* Ground check from the tutorial
-    public void GroundCheck()
-    {
-        Collider[] colliders;
-
-        Vector3 spherePosition = transform.position + sphereOffset;
-        colliders = Physics.OverlapSphere(spherePosition, sphereRadius, notPlayerLayer);
-
-        if (colliders.Length == 0)
-        {
-            isGrounded = false;
-            Debug.Log("No colliders detected.");
-        }
-
-        if (colliders.Length > 0)
-        {
-            foreach (Collider col in colliders)
-            {
-                Vector3 colliderPosition = col.gameObject.transform.position;
-
-                float groundAngle = Vector3.Angle(spherePosition, colliderPosition);
-
-                if (groundAngle > 180f + maxSlopeAngle || groundAngle < 180f - maxSlopeAngle)
-                {
-                    isGrounded = true;
-                }
-            }
-        }
-    }
-*/
 }
